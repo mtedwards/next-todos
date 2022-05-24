00263ac91d41e5d632f1b38726b1e9e3ea981fe7
Matt Edwards <matt@emptyhead.com.au>
2022-05-24 21:58:25 +0930

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SUPABASE_URL:'https://pvyxprmdaieaagtxknre.supabase.co',
+    SUPABASE_ANON_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2eXhwcm1kYWllYWFndHhrbnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTMzNTI1ODQsImV4cCI6MTk2ODkyODU4NH0.WAJwP66SXDvto4LcM2vTVzbTHN_drW2mDsUOoCrrkCs',
+  }, 
 }
 
 module.exports = nextConfig
+
+
